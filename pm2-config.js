a743aab4a5c9dfbb454237091c2f2e864dda6ba3
roshan <roshanroshuff@gmail.com>
2024-06-28 12:43:57 +0530

    updated pm2 config file

--- pm2-config.js
+++ pm2-config.js
@@ -2,11 +2,11 @@ module.exports = {
     apps: [{
       name: 'paintcont-service-backend',
       script: 'src/app.ts',
-      interpreter: './node_modules/.bin/ts-node',
+      interpreter: 'node',
+      interpreter_args: '-r ts-node/register',
       env: {
         NODE_ENV: 'production',
         PORT: 7777,
       }
     }]
-  };
-  
+  };  
