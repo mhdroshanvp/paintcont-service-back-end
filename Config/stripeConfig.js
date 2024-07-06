c738a6e2a0733db75c3e95d92df8f06824ac4a9b
roshan <roshanroshuff@gmail.com>
2024-07-06 17:34:57 +0530

    deleted js  files

--- Config/stripeConfig.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const stripe_1 = __importDefault(require("stripe"));
-const stripe = new stripe_1.default('sk_test_51PZThc2LBBR2nOfKO77VL59K9kx7aSgoJBWRWKCZ3wYllE5yfWr9yia58j7IJ7TYmXEKcYIyq9oLBxmslA8egQ4H00TBf7w0W5');
-exports.default = stripe;
-//# sourceMappingURL=stripeConfig.js.map
